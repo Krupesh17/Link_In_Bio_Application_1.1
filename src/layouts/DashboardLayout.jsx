7322b61a378011ae089cfcbfa029a47e91681cad
Krupesh17 <krupeshparab17@gmail.com>
2025-05-26 02:34:17 +0530

    Make changes in DashboardLayout.jsx and Dashboard.jsx files.

--- src/layouts/DashboardLayout.jsx
+++ src/layouts/DashboardLayout.jsx
@@ -1,12 +1,20 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
-import { BottomTabBar, DashboardHeader } from "@/components";
+import { BottomTabBar, DashboardHeader, MobilePreview } from "@/components";
+import { ScrollArea } from "@/components/ui/scroll-area";
 
 const DashboardLayout = () => {
   return (
     <div className="h-dvh w-full flex flex-col overflow-hidden">
       <DashboardHeader />
-      <Outlet />
+      <div className="flex-1 w-full grid grid-cols-[1fr_550px] max-xl:grid-cols-1 overflow-hidden">
+        <ScrollArea className="h-full w-full px-5 max-sm:px-2.5">
+          <Outlet />
+        </ScrollArea>
+        <aside className="relative h-full w-full border-l border-l-border flex items-center max-xl:hidden">
+          <MobilePreview />
+        </aside>
+      </div>
       <BottomTabBar />
     </div>
   );
--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,27 +1,15 @@
 import React, { useState } from "react";
-import {
-  MobilePreview,
-  ProfileInfoHeader,
-  URLBox,
-  LinkGroup,
-} from "@/components";
-import { ScrollArea } from "@/components/ui/scroll-area";
+import { ProfileInfoHeader, URLBox, LinkGroup } from "@/components";
 
 const Dashboard = () => {
   const [linksNewPositionUpdating, setLinksNewPositionUpdating] =
     useState(false);
 
   return (
-    <div className="flex-1 w-full grid grid-cols-[1fr_550px] max-xl:grid-cols-1 overflow-hidden">
-      <ScrollArea className="h-full w-full px-5 max-sm:px-2.5">
-        <URLBox />
-        <ProfileInfoHeader linksUpdating={linksNewPositionUpdating} />
-        <LinkGroup setLinksNewPositionUpdating={setLinksNewPositionUpdating} />
-      </ScrollArea>
-
-      <aside className="relative h-full w-full border-l border-l-border flex items-center max-xl:hidden">
-        <MobilePreview />
-      </aside>
+    <div>
+      <URLBox />
+      <ProfileInfoHeader linksUpdating={linksNewPositionUpdating} />
+      <LinkGroup setLinksNewPositionUpdating={setLinksNewPositionUpdating} />
     </div>
   );
 };
