38ed452f3aea4f45db7f3340477adc7a957f8ef2
Krupesh17 <krupeshparab17@gmail.com>
2025-07-30 13:30:00 +0530

    Updated 'UserLandingProductSection'.

--- src/components/UserLandingProductSection.jsx
+++ src/components/UserLandingProductSection.jsx
@@ -4,7 +4,7 @@ import { UserLandingProductButton } from ".";
 const UserLandingProductSection = ({ productsData, buttonAppearance }) => {
   return (
     <div className="mt-5 min-h-10">
-      <ul className="grid grid-cols-[repeat(auto-fill,_minmax(180px,_1fr))] gap-4 px-2.5">
+      <ul className="grid grid-cols-[repeat(auto-fill,_minmax(160px,_1fr))] gap-4 px-2.5">
         {productsData?.map((product) => {
           if (!product?.product_published) return;
           return (
